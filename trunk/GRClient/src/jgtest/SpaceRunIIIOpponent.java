9670d4505d4d0b196e2ee009d94370eb6159ba22
mikaelbre <mikaelbre@2affd191-2e6e-88e4-908c-3c631a8fcab9>
2011-01-07 13:18:42 +0000

    Fixed ViewOffset bug?

--- trunk/GRClient/src/jgtest/SpaceRunIIIOpponent.java
+++ trunk/GRClient/src/jgtest/SpaceRunIIIOpponent.java
@@ -69,6 +69,7 @@ public class SpaceRunIIIOpponent extends StdGame {
         String[] map = LevelDesign.TEST_LEVEL;
         setTilesMulti(0, 0, map);
         player = new Player(32, pfHeight() / 2 - 100, 3, this);
+        setViewOffset((int) getObject("player").x + 100, (int) getObject("player").y, true);
     }
 
     public Player getPlayer() {
