6de83597c54904b407a7d637933b99d4307f352f
mikaelbre <mikaelbre@2affd191-2e6e-88e4-908c-3c631a8fcab9>
2011-01-10 18:18:24 +0000

    Major structural changes. Packages, libraries, etc.

---
+++ trunk/GRServer/src/com/gameroulette/server/GameHostException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gameroulette.server;
+
+/**
+ *
+ * @author mikaelbrevik
+ */
+public class GameHostException extends Exception {
+
+    public GameHostException(String msg) {
+        super(msg);
+    }
+}
---
+++ trunk/GRServer/src/com/gameroulette/server/HighscoreEntryImpl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gameroulette.server;
+
+import rmi.stubbs.HighscoreEntry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author mikaelbrevik
+ */
+public class HighscoreEntryImpl extends UnicastRemoteObject implements HighscoreEntry {
+
+    private String name;
+    private int score;
+
+    public HighscoreEntryImpl(String name, int score) throws RemoteException {
+        this.name = name;
+        this.score = score;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    public String toString() {
+        return name + ": " + score;
+    }
+}
